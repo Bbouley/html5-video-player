0b277c3cbc96e6b66675d26ecaf6ad1c6defd0e7
bbouley <b.bouley@hotmail.co.uk>
2016-02-20 19:18:27 -0700

    CRUD routes finished

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,14 +5,63 @@ var Video = require('../models/videos');
 
 
 // GET all videos
+router.get('/videos', function(req, res) {
+    Video.findQ()
+    .then(function(data) {
+        res.status(200).json(data);
+    })
+    .catch(function(err) {
+        res.status(500).send({'ERROR' : err});
+    });
+})
 
 // GET single videos
+router.get('/video/:id', function(req, res) {
+    Video.findByIdQ(req.params.id)
+    .then(function(data) {
+        res.status(200).json(data);
+    })
+    .catch(function(err) {
+        res.status(500).send({'ERROR' : err});
+    });
+})
 
 // POST single video
+router.post('/videos', function(req, res) {
+    var newVideo = new Video(req.boy);
+    newVideo.saveQ()
+    .then(function(data) {
+        res.status(200).json(data);
+    })
+    .catch(function(err) {
+        res.status(500).send({'ERROR' : err});
+    });
+});
 
 // PUT single video
+router.put('/video/:id', function(req, res) {
+    var id = req.params.id;
+    var update = req.body;
+    var options = {new : true};
+
+    video.findByIdAndUpdateQ(id, update, options)
+    .then(function(data) {
+        res.status(200).json(data);
+    })
+    .catch(function(err) {
+        res.status(500).send({'ERROR' : err});
+    });
+});
 
 // DELETE single video
-
+router.delete('/video/:id', function(req, res) {
+    router.findByIdAndRemoveQ(req.params.id)
+    .then(function(data) {
+        res.status(200).json(data);
+    })
+    .catch(function(err) {
+        res.status(500).send({'ERROR' : err});
+    });
+});
 
 module.exports = router;
